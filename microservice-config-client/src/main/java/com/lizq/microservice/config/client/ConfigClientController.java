f13979cf89b1dacb3033f3116754f71d9d5b5638
lizq <ghappier@gmail.com>
2017-10-11 18:27:24 +0800

    加入配置中心客户端
    加入配置中心客户端

---
+++ microservice-config-client/src/main/java/com/lizq/microservice/config/client/ConfigClientController.java
@@ -0,0 +1,19 @@
+package com.lizq.microservice.config.client;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class ConfigClientController {
+	
+	@Value("${from}")
+	private String from;
+	
+	@GetMapping("/from")
+	public String from() {
+		return this.from;
+	}
+}
--- shutdown.cmd
+++ shutdown.cmd
@@ -22,3 +22,6 @@ TASKKILL /FI "WINDOWTITLE eq gateway*" /IM cmd.exe /F /T
 %SLEEP% 1 > nul
 
 TASKKILL /FI "WINDOWTITLE eq configserver*" /IM cmd.exe /F /T
+%SLEEP% 1 > nul
+
+TASKKILL /FI "WINDOWTITLE eq configclient*" /IM cmd.exe /F /T
